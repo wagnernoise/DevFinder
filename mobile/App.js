da5a5334e5a432ee1f1f662d449e7e412f0e65d2
Wagner Rosa <wagner.o.rosa@gmail.com>
2020-01-18 17:07:22 +0100

    start expo mobile

---
+++ mobile/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+import Routes from './src/routes';
+
+export default function App() {
+  return (
+    <Routes />
+  );
+}
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#7159c1',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+//   title :{
+//     fontSize: 32,
+//     fontWeight: "bold",
+//     color: "#fff"
+//   },
+// });
